26036b1d7636108d4f947869c7b6f4de330fead0
nebr0k <90468237+nebr0k@users.noreply.github.com>
2022-04-06 16:58:48 +0300

    Add files via upload

---
+++ 8.4.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int Count(const string s)
+{
+	int k = 0;
+	size_t pos = 0;
+	while ((pos = s.find('a', pos)) != -1)
+	{
+		pos++;
+		if (s[pos] == 'b')
+			pos++;
+		if (s[pos] == 'c')
+			k++;
+	}
+	return k;
+}
+string Change(string& s)
+{
+	size_t pos = 0;
+	while ((pos = s.find("abc", pos)) != -1)
+	{
+		s.replace(pos, 3, "**");
+	}
+	return s;
+}
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	cout << "Number of occurrences " << Count(str) << endl;
+	string dest = Change(str);
+	cout << "Modified string: " << dest << endl;
+
+		return 0;
+}
